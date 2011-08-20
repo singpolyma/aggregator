8d4b1987d0792c76817523ad3071ed99768b2999
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-08-20 00:25:31 +0000

    maildir creation scripts

---
+++ lib/make_mime.rb
@@ -0,0 +1,65 @@
+# encoding: utf-8
+BOUNDARY = 'naeburaxurkbxrcubxkrcbxrgapbxurcbpucbiuhxbs4409573940985672064'
+
+require 'html2markdown'
+require 'htmlentities'
+
+def make_mime(meta, item)
+	item[:author] ||= meta[:author] if meta[:author]
+	if meta[:logo]
+		item[:author] ||= {}
+		item[:author][:logo] ||= meta[:logo]
+	end
+	item[:author][:photo] ||= item[:author][:logo] if item[:author] && item[:author][:logo]
+
+	r = 'From '
+	if item[:author][:email]
+		r << item[:author][:email]
+	end
+	r << ' ' << item[:published].rfc2822 << "\n"
+
+	r = ''
+
+	r << "Message-ID: <#{item[:id] || item[:bookmark] || "#{Time.now.to_i}@no-id.example.com"}>\n"
+	r << "Subject: #{(item[:title].to_s != '' ? item[:title] : (HTMLEntities.decode_entities(item[:content].gsub(/<[^>]*>/, ''))[0,50] + '…')).gsub(/\s/, ' ')}\n"
+	r << "Content-Location: <#{item[:bookmark]}>\n" if item[:bookmark]
+	r << "From: \"#{item[:author][:fn]}\" <#{item[:author][:email]}>\n"
+	r << "X-URL: #{item[:author][:url]}\n" if item[:author][:url]
+	r << "X-Image-URL: #{item[:author][:photo]}\n" if item[:author][:photo]
+	r << "Date: #{item[:published].rfc2822}\n"
+
+	(item[:in_reply_to] || []).each do |parent|
+		r << "In-Reply-To: <#{parent[:ref]}>\n" if parent[:ref]
+		r << "In-Reply-To: <#{parent[:href]}>\n" if parent[:href]
+	end
+
+	r << "X-Originally-From: \"#{meta[:title]}\" <#{meta[:self]}>\n"
+
+	r << "Content-Type: multipart/alternative; boundary=#{BOUNDARY}\n"
+	r << "Content-Transfer-Encoding: 8bit\n"
+
+	r << "\n"
+
+	
+	r << "--#{BOUNDARY}\n"
+	r << "Content-Type: text/plain; charset=utf-8\n"
+	r << "Content-Transfer-Encoding: 8bit\n"
+	r << "\n"
+	r << HTML2Markdown.new(item[:content].to_s.gsub(/\s+/, ' ')).to_s.sub(/^\n*/, '').sub(/\n*$/, '')
+
+	r << "\n\n"
+	
+	r << "--#{BOUNDARY}\n"
+	r << "Content-Type: text/html; charset=utf-8\n"
+	r << "Content-Transfer-Encoding: 8bit\n"
+	r << "\n"
+	r << item[:content].to_s
+
+	r << "\n\n"
+
+	r << "--#{BOUNDARY}\n"
+	r << "Content-Type: application/xml; charset=utf-8\n"
+	r << "Content-Transfer-Encoding: 8bit\n"
+	r << "\n"
+	r << item[:xml].to_s
+end
---
+++ update_maildir.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+$: << File.dirname(__FILE__) + '/lib'
+require 'make_mime'
+require 'from_hatom'
+require 'fileutils'
+
+maildir, state_file = ARGV
+saw = (open(state_file).read rescue '')
+to_write = saw
+wrote_state = false
+
+HOSTNAME = `hostname`.chomp
+
+i = 0
+from_hatom(STDIN.read).each do |entry|
+	break if entry[:item][:id] == saw
+
+	unless wrote_state
+		to_write = entry[:item][:id]
+		wrote_state = true
+	end
+
+	name = "#{Time.now.to_f}_#{$$}_#{HOSTNAME}_#{i}"
+	tmp = File.join(maildir, 'tmp', name)
+	new = File.join(maildir, 'new', name)
+	open(tmp, 'w') {|fh|
+		fh.write make_mime(entry[:meta], entry[:item])
+	}
+	FileUtils.ln tmp, new
+	FileUtils.rm tmp
+
+	i += 1
+end
+
+# Do not write until end so that we try again on fail
+open(state_file, 'w') {|fh| fh.write(to_write)}
