12e478720fe9fa021de6b36203088e0798959947
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-09-08 10:37:39 -0500

    Get all child text

--- lib/xml_feed_polyglot.rb
+++ lib/xml_feed_polyglot.rb
@@ -5,6 +5,10 @@ require 'time'
 $: << File.dirname(__FILE__)
 require 'util'
 
+def txt(el)
+	el.texts.join(' ')
+end
+
 def xml_feed_polyglot(string)
 	root = REXML::Document.new(string).root
 
@@ -22,19 +26,19 @@ def xml_feed_polyglot(string)
 	root.add_namespace('thr', 'http://purl.org/syndication/thread/1.0')
 
 	root.elements.each('./title') {|el| meta[:title] = ''; el.write(meta[:title]); meta[:title].gsub!(/<[^>]+>/, '').strip!}
-	root.elements.each('./link') {|el| meta[:link] = el.attributes['href'] ? el.attributes['href'].to_s : el.text; break }
+	root.elements.each('./link') {|el| meta[:link] = el.attributes['href'] ? el.attributes['href'].to_s : txt(el); break }
 	root.elements.each('./atom:link[@rel="self"]') {|el| meta[:self] = el.attributes['href'] }
 
-	root.elements.each('./image/url|./logo') {|el| meta[:logo] = el.text}
+	root.elements.each('./image/url|./logo') {|el| meta[:logo] = txt(el)}
 
 	root.elements.each('./atom:author|./author|./dc:creator') {|el|
 		n = el.children.inject(0) {|n,c| c.class != REXML::Text ? n+1 : n }
 		if n > 0
 			meta[:author] = {}
-			el.elements.each('./name') {|c| meta[:author][:fn] = c.text}
-			el.elements.each('./uri') {|c| meta[:author][:url] = c.text}
+			el.elements.each('./name') {|c| meta[:author][:fn] = txt(c)}
+			el.elements.each('./uri') {|c| meta[:author][:url] = txt(c)}
 		else
-			meta[:author] = {:fn => el.text}
+			meta[:author] = {:fn => txt(el)}
 		end
 	}
 
@@ -55,21 +59,21 @@ def xml_feed_polyglot(string)
 				el.write(item[:title])
 				item[:title].gsub!(/<[^>]+>/, '').strip!
 			elsif el.attributes['type'].to_s == 'html'
-				item[:title] = el.text
+				item[:title] = txt(el)
 			else
-				item[:title] = el.text
+				item[:title] = txt(el)
 			end
 		}
-		itemel.elements.each('./link') {|el| item[:bookmark] = el.text}
+		itemel.elements.each('./link') {|el| item[:bookmark] = txt(el)}
 		itemel.elements.each('./atom:link[@rel="alternate"][@type="text/html"]') {|el| item[:bookmark] = el.attributes['href']}
-		itemel.elements.each('./guid|./atom:id') {|el| item[:id] = el.text}
+		itemel.elements.each('./guid|./atom:id') {|el| item[:id] = txt(el)}
 		itemel.elements.each('./description') {|el|
 			# Always end up HTML-safe
-			item[:content] = el.text
+			item[:content] = txt(el)
 		}
 		itemel.elements.each('./content:encoded') {|el|
 			# Always end up HTML-safe
-			item[:content] = el.text
+			item[:content] = txt(el)
 		}
 		itemel.elements.each('./atom:content') {|el|
 			item[:content] = ''
@@ -87,29 +91,29 @@ def xml_feed_polyglot(string)
 			n = el.children.inject(0) {|n,c| c.class != REXML::Text ? n+1 : n }
 			if n > 0
 				item[:author] = {}
-				el.elements.each('./name') {|c| item[:author][:fn] = c.text}
-				el.elements.each('./uri') {|c| item[:author][:url] = c.text}
+				el.elements.each('./name') {|c| item[:author][:fn] = txt(c)}
+				el.elements.each('./uri') {|c| item[:author][:url] = txt(c)}
 			else
-				item[:author] = {:fn => el.text}
+				item[:author] = {:fn => txt(el)}
 			end
 		}
-		itemel.elements.each('./pubDate|./dc:date|./published') {|el| item[:published] = Time.parse(el.text)}
+		itemel.elements.each('./pubDate|./dc:date|./published') {|el| item[:published] = Time.parse(txt(el))}
 
-		itemel.elements.each('./category|./atom:category') {|el| item[:category] << (el.text || el.attributes['term'])}
+		itemel.elements.each('./category|./atom:category') {|el| item[:category] << (txt(el) || el.attributes['term'])}
 		itemel.elements.each('./thr:in-reply-to') {|el| item[:in_reply_to] << {:ref => el.attributes['ref'], :href => el.attributes['href']}}
 
 		itemel.elements.each('./source') {|el|
 			item[:source] = {
 				:id    => el.attributes['url'],
 				:self  => el.attributes['url'],
-				:title => el.text
+				:title => txt(el)
 			}
 		}
 
 		itemel.elements.each('./atom:source') {|el|
 			item[:source] = {}
-			el.elements.each('./id') {|c| item[:source][:id] = c.text}
-			el.elements.each('./title') {|c| item[:source][:title] = c.text}
+			el.elements.each('./id') {|c| item[:source][:id] = txt(c)}
+			el.elements.each('./title') {|c| item[:source][:title] = txt(c)}
 			el.elements.each('./atom:link[@rel="self"]') {|c| item[:source][:self] = c.attributes['href']}
 		}
 
